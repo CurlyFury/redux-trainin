cf6c1138a142bc25cebb023407b408510634f578
Ilija <nukawanderer@gmail.com>
2022-01-26 11:46:24 +0800

    base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./index.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash.cash);
+  const customers = useSelector((state) => state.customers.customers);
+  console.log(customers);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  const getMoney = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now(),
+    };
+    dispatch({ type: "ADD_CUSTOMER", payload: customer });
+  };
+  const removeCustomer = (customer) => {
+    dispatch({ type: "REMOVE_CUSTOMERS", payload: customer.id });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="block">
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <div style={{ paddingRight: "10px", fontSize: "45px" }}>{cash}</div>
+        <button onClick={() => addCash(Number(prompt("Введите сумму")))}>
+          Добавить
+        </button>
+        <button onClick={() => getMoney(Number(prompt("Введите сумму")))}>
+          Убавить
+        </button>
+        <button onClick={() => addCustomer(prompt("Введите имя"))}>
+          Добавить пользователя
+        </button>
+        <button onClick={() => getMoney(Number(prompt("Введите сумму")))}>
+          Удалить пользователя
+        </button>
+      </div>
+      {customers.length > 0 ? (
+        <div>
+          {customers.map((customer) => (
+            <div onClick={() => removeCustomer(customer)}>{customer.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div style={{ fontSize: "2rem", paddingTop: "10px" }}>
+          Нет клиентов !
+        </div>
+      )}
     </div>
   );
 }
